1e43ee7f8990c7cb1ee98b593b3c677e9f49c9da
Dilek Eren <108549514+cheekyuser@users.noreply.github.com>
2022-09-28 18:57:41 +0300

    Add files via upload

---
+++ javaCamp2.Hafta/methods2.java
@@ -0,0 +1,13 @@
+public class methods2 {
+    public static void main(String[] args) {
+        int addition = addition(6,12);
+        String message = message("Girilen iki sayının toplamı: ");
+        System.out.println(message+addition);
+
+
+}    public static int addition(int num1,int num2){
+        return num1+num2;
+    }public static String message(String text){
+        return text;
+    }
+}
