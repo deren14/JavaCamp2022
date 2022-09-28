66cf7158afc84ab14eeb0c66317c313c34d7ee06
Dilek Eren <108549514+cheekyuser@users.noreply.github.com>
2022-09-28 18:22:07 +0300

    Add files via upload

---
+++ javaCamp2.Hafta/ders.25_methods2/src/methods.java
@@ -0,0 +1,9 @@
+public class methods {
+    public static void main(String[] args) {
+        findSquare(9);
+    }
+    public static void findSquare(int num){
+        int square = num*num;
+        System.out.println(square);
+    }
+}
